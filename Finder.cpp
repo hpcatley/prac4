f5d416379c5bea0c11dc298390cb958f6080c092
hpcatley <hpcatley@gmail.com>
2023-08-26 17:59:35 +0930

    Attempt 3

--- Finder.cpp
+++ Finder.cpp
@@ -9,8 +9,6 @@
         for(size_t i = 1; i <= s2.size(); i++) {
             size_t found = s1.find(s2.substr(0, i));
             if (found != string::npos) {
-                result.push_back(found);
-            } else {
                 result.push_back(-1);
             }
         }
