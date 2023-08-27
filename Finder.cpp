c0639a0b807aa49b5e879ef87fe94a8e6e60ffd2
hpcatley <hpcatley@gmail.com>
2023-08-27 19:09:16 +0930

    Attempt 11

--- Finder.cpp
+++ Finder.cpp
@@ -3,16 +3,18 @@
 #include <string>
 #include <iostream>
 
-    using namespace std;
+using namespace std;
 
-    vector<int> Finder::findSubstrings(string s1, string s2) {
+vector<int> Finder::findSubstrings(string s1, string s2) {
 
         vector<int> result;
+        int previous = 0;
 
         for(size_t i = 1; i <= s2.size(); i++) {
-            size_t found = s1.find(s2.substr(0, i));
+            size_t found = s1.find(s2.substr(0, i), previous);
             if (found != string::npos) {
                 result.push_back(found);
+                previous = found;
             } else {
                 result.push_back(-1);
                 break;
